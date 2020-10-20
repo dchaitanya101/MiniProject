84ca361bdde80d47f368bdbfc451263b4ec3b8bd
dchaitanya101 <deshpandechaitanya98@gmail.com>
2020-10-20 17:31:37 +0530

    Done with login and register

---
+++ conn.php
@@ -0,0 +1,12 @@
+<?php
+
+    $servername = "localhost";
+    $dbname = "customermgmt";
+    $user = "root";
+    $password = "admin123";
+
+    $conn = new mysqli($servername, $user, $password, $dbname);
+
+    
+
+?>
